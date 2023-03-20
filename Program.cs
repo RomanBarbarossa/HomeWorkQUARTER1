a1f13ed1114f1cf2e77187817f3c5c8eb6a9d6b2
roman.barbarossa <roman.barbarossa@iMac.local>
2023-03-20 16:25:45 +0300

    task is done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿
-
-
-
 string[] example = new string[] {"hello", "2", "world",";-)","www"};
+int size = 0;
 for (int i = 0; i < example.Length; i++)
 {
-    if (example[i] <= 0)
+    if (example[i].Length <= 3)
     {
-        
+        size++;
     }
 }
+
+string[] result = new string [size];
+int j = 0;
+ for (int i = 0; i < example.Length; i++)
+ {
+    if (example[i].Length <= 3)
+    {
+        result[j] = example[i];
+        j++;
+    }
+ }
+Console.WriteLine($"[{String.Join("|",example)}]");
+Console.WriteLine($"[{String.Join("|",result)}]");
+
